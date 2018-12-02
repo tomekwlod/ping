134418413d727592b37ff255b0203a3c3cb60733
Tomasz Wlodarczyk <twl@phase-ii.com>
2018-12-02 22:36:59 +0000

    LastStatus missing when updating document

--- cmd/server/handler.go
+++ cmd/server/handler.go
@@ -218,6 +218,7 @@ func (s *service) updatepageHandler(w http.ResponseWriter, r *http.Request) {
 	update.Data.Name = body.Data.Name
 	update.Data.Url = body.Data.Url
 	update.Data.RescueUrl = body.Data.RescueUrl
+	update.Data.LastStatus = body.Data.LastStatus
 
 	update.Data.Id = bson.ObjectIdHex(params.ByName("id"))
 	update.Data.SetUpdateDefaults(time.Now())
